ce41eac923559f3557712140d3f7ccc7e7723831
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-06-24 20:11:58 +0400

    Add stub room-climate-regulator service and task do

--- src/main/java/ge/edu/freeuni/sdp/iot/service/room_climate_regulator/core/service/RoomClimateRegulatorService.java
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/room_climate_regulator/core/service/RoomClimateRegulatorService.java
@@ -1,5 +1,47 @@
 package ge.edu.freeuni.sdp.iot.service.room_climate_regulator.core.service;
 
+import ge.edu.freeuni.sdp.iot.service.room_climate_regulator.core.ServiceState;
+import ge.edu.freeuni.sdp.iot.service.room_climate_regulator.core.proxy.*;
 
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/houses/{house_id}/floors/{floor_id}/task")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class RoomClimateRegulatorService {
+
+    @PUT
+    public Response update(@PathParam("house_id") String houseId, @PathParam("floor_id") String floorId,
+                           @NotNull RoomClimateRegulatorTaskDo task) {
+        // TODO add real implementation.
+        return Response.ok().build();
+    }
+
+    protected ServiceState getServiceState() {
+        return ServiceState.DEV;
+    }
+
+    protected AirConditioningSwitchProxy getAirConditioningSwitch() {
+        return new AirConditioningSwitchProxy(getServiceState());
+    }
+
+    protected HeatingSwitchServiceProxy getHeatingSwitch() {
+        return new HeatingSwitchServiceProxy(getServiceState());
+    }
+
+    protected HouseRegistryServiceProxy getHouseRegistryService() {
+        return new HouseRegistryServiceProxy(getServiceState());
+    }
+
+    protected HumanAtHomeServiceProxy getHumanAtHomeService() {
+        return new HumanAtHomeServiceProxy(getServiceState());
+    }
+
+    protected RoomThermometerProxy getRoomThermometer() {
+        return new RoomThermometerProxy(getServiceState());
+    }
 }
---
+++ src/main/java/ge/edu/freeuni/sdp/iot/service/room_climate_regulator/core/service/RoomClimateRegulatorTaskDo.java
@@ -0,0 +1,32 @@
+package ge.edu.freeuni.sdp.iot.service.room_climate_regulator.core.service;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoomClimateRegulatorTaskDo {
+
+    private Double temperature;
+
+    private Integer period;
+
+    public RoomClimateRegulatorTaskDo(Double temperature, Integer period) {
+        this.temperature = temperature;
+        this.period = period;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Integer getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Integer period) {
+        this.period = period;
+    }
+}
